43dc681f5873f0794c510a7555235b641ecf205c
Erich Smith <erich.smith@lifeomic.com>
2019-09-13 17:33:35 -0400

    Extract into open-source module

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# bitbucket-pr-detector
-Detect novel Pull Requests and alert on them
+# Bitbucket PullRequest Detector
+
+Pull Requests increasingly dominate our technical workflows, yet ensuring that
+the right people review them, or that we have a centralized place to configure
+automation side-effects in response to new Pull Requests, can be challenging.
+
+This module, intended to be run periodically, checks to see if new pull requests
+have been opened in one or more target bitbucket repositories.
+
+If those PRs contain changes matching a certain pattern, this script will
+perform actions, e.g. sending a slack message to a configured channel.
+
+## Configuration and Usage
+
+Import and execute the module via:
+
+```typescript
+import { processPullRequestsAsync, PRDetectorConfig } from '@jupiterone/bitbucket-pr-detector';
+
+const config: PRDetectorConfig = {
+  ...
+};
+
+await processPullRequestsAsync(config);
+```
+
+Where `config` satisfies the interface:
+
+```javascript
+export interface PRDetectorConfig {
+    bitbucketOrg: string       // required, organization name
+    bitbucketRepo: string      // required, repository name
+    bitbucketUsername: string  // required, bitbucket user name
+    bitbucketPassword: string  // required, bitbucket password
+    slackWebhookUrl?: string   // if given, will alert to Slack
+    slackAuthor?: string       // alert author
+    slackAlertTitle?: string   // alert title
+    detectPath?: string        // required if using default detectionFilter(), which
+                               // returns true if any PR modified paths match string
+    detectionFilter?: (config: PRDetectorConfig, diffStats: any[]) => boolean
+    checkPRSeenAsync?: (config: PRDetectorConfig, input: PRSeenInput) => Promise<boolean>
+    savePRSeenAsync?: (config: PRDetectorConfig, input: PRSaveInput) => Promise<void>
+    processPRAsync?: (config: PRDetectorConfig, pr: any) => Promise<void>
+    logger?: any
+}
+```
+
+### Detection filtering
+
+Unless `detectionFilter()` is overridden, the module will search for Pull
+Requests which contain changed files whose path includes a substring specified
+by the `detectPath` config attribute. For instance, if you have a `wiki`
+repository, and it contains RFCs organized under a `docs/RFCs` folder, then
+specifying `detectPath: 'docs/RFCs'` will detect new Pull Requests that contain
+RFC documentation.
+
+The `detectionFilter` function receives the config object, the Pull Request
+Object, and an array of Bitbucket API DiffStat objects for that PR.
+
+See:
+
+- [Bitbucket API PullRequest Documentation](https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D/pullrequests#get)
+- [Bitbucket API DiffStat Documentation](https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D/diffstat/%7Bspec%7D)
+
+The function should return true if that PR is considered novel or worth alerting
+on.
+
+### Alerting to Slack
+
+If you'd like the module to alert to a Slack channel whenever a novel Pull
+Request is detected, configure the `slackWebhookUrl` with the custom integration
+webhook URL for a channel of your choice. You'll also want to configure
+`slackAuthor` and `slackAlertTitle` appropriately.
+
+For instance, continuing the example above, we might specify:
+
+```typescript
+{
+  slackWebhookUrl: process.env.SLACK_WEBHOOK_RFCS_CHANNEL,
+  slackAuthor: 'new-rfc-notifier',
+  slackAlertTitle: 'New RFC Pull Request found in wiki repository'
+}
+```
+
+If you'd rather not alert to Slack, omit the `slackWebhookUrl` attribute.
+
+### Persistence / Alert Deduping
+
+Since this is intended to run periodically, a persistence store should be made
+available to cache previously seen Pull Requests, to prevent duplicate
+alerts/actions for the same triggering Pull Request. A key-value store like
+Redis or DynamoDB is ideal, but any persistence mechanism which satisfies the
+`checkPRSeenAsync()/savePRSeenAsync()` function interfaces will work. The module
+will complain if you have not overridden the default persistence functions.
+
+### Additional Processing
+
+If you'd like to take other actions on new Pull Requests which satisfy the
+`detectionFilter()`, override the `processPRAsync()` attribute with a custom
+function.
---
+++ src/index.ts
@@ -0,0 +1,174 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { logger as defaultLogger } from './logging';
+import * as slack from './slack';
+
+export interface PRDetectorConfig {
+  bitbucketOrg: string;
+  bitbucketRepo: string;
+  bitbucketUsername: string;
+  bitbucketPassword: string;
+  slackWebhookUrl?: string;
+  slackAuthor?: string;
+  slackAlertTitle?: string;
+  detectPath?: string;
+  detectionFilter?: (
+    config: PRDetectorConfig,
+    pr: any,
+    diffStats: any[]
+  ) => boolean;
+  checkPRSeenAsync?: (
+    config: PRDetectorConfig,
+    input: PRSeenInput
+  ) => Promise<boolean>;
+  savePRSeenAsync?: (
+    config: PRDetectorConfig,
+    input: PRSaveInput
+  ) => Promise<void>;
+  processPRAsync?: (config: PRDetectorConfig, pr: any) => Promise<void>;
+  logger?: any;
+}
+
+interface PRSeenInput {
+  prNumber: string;
+}
+
+interface PRSaveInput extends PRSeenInput {
+  seen: number;
+}
+
+export async function processPullRequestsAsync(
+  config: PRDetectorConfig
+): Promise<void> {
+  if (!config.logger) {
+    config.logger = defaultLogger;
+  }
+  if (config.slackWebhookUrl) {
+    slack.init(config.slackWebhookUrl);
+  }
+
+  const prs = await gatherAPIValues(config, 'pullrequests?state=OPEN');
+
+  debugLog(config, `found ${prs.length} open pull requests`);
+
+  const {
+    checkPRSeenAsync = defaultCheckPRSeenAsync,
+    savePRSeenAsync = defaultSavePRSeenAsync,
+    detectionFilter = defaultDetectionFilter,
+    processPRAsync = defaultProcessPRAsync,
+  } = config;
+
+  // filter for previously unseen PRs that satisfy detection criteria
+  const newRFCs: any = [];
+
+  for (const pr of prs) {
+    if (!(await checkPRSeenAsync(config, { prNumber: pr.id }))) {
+      debugLog(config, `pr ${pr.id} seems new...`);
+      const dstHash = pr.destination.commit.hash;
+      const srcHash = pr.source.commit.hash;
+      const diffStats = await gatherAPIValues(
+        config,
+        `diffstat/${dstHash}..${srcHash}`
+      );
+      if (detectionFilter(config, pr, diffStats)) {
+        debugLog(config, `pr ${pr.id} modified an RFC...`);
+        newRFCs.push(pr);
+      }
+      await savePRSeenAsync(config, {
+        prNumber: pr.id,
+        seen: Math.floor((new Date() as any) / 1000),
+      });
+      debugLog(config, `cached ${pr.id} as seen...`);
+    }
+  }
+
+  for (const pr of newRFCs) {
+    if (config.slackWebhookUrl) {
+      await notifyViaSlack(config, pr);
+    }
+    await processPRAsync(config, pr);
+  }
+}
+
+async function gatherAPIValues(
+  config: PRDetectorConfig,
+  apiUrl: string
+): Promise<any[]> {
+  const BB_API_V2_BASE = `https://api.bitbucket.org/2.0/repositories/${config.bitbucketOrg}/${config.bitbucketRepo}`;
+  const bbReqAuth: AxiosRequestConfig = {
+    auth: {
+      username: config.bitbucketUsername,
+      password: config.bitbucketPassword,
+    },
+  };
+
+  let prs: any = [];
+  let url = `${BB_API_V2_BASE}/${apiUrl}`;
+  let res;
+
+  // gather possibly paginated data for bitbucket API values
+  do {
+    res = await axios.get(url, bbReqAuth);
+    prs = prs.concat(res.data.values);
+    url = res.data.next;
+  } while (url);
+
+  return prs;
+}
+
+async function defaultCheckPRSeenAsync(
+  config: PRDetectorConfig,
+  input: PRSeenInput
+): Promise<boolean> {
+  config.logger.error('You should implement and configure this function');
+  return false;
+}
+
+async function defaultSavePRSeenAsync(
+  config: PRDetectorConfig,
+  input: PRSaveInput
+): Promise<void> {
+  config.logger.error('You should implement and configure this function');
+  return;
+}
+
+async function defaultProcessPRAsync(
+  config: PRDetectorConfig,
+  pr: any
+): Promise<void> {
+  return;
+}
+
+function defaultDetectionFilter(
+  config: PRDetectorConfig,
+  pr: any,
+  diffStats: any[]
+): boolean {
+  if (!config.detectPath) {
+    throw new Error(
+      'you must configure the detectPath attribute when using the default detection filter'
+    );
+  }
+  return diffStats.some(
+    (i: any) => ((i.new || {}).path || '').indexOf(config.detectPath) !== -1
+  );
+}
+
+async function notifyViaSlack(
+  config: PRDetectorConfig,
+  pr: any
+): Promise<void> {
+  debugLog(config, `alerting slack for ${pr.id}: ${pr.title}`);
+  await slack.send({
+    author: pr.author.display_name,
+    title: pr.title,
+    titleLink: pr.links.html.href,
+    source: config.slackAuthor,
+    alertTitle: config.slackAlertTitle,
+  });
+}
+
+function debugLog(config: PRDetectorConfig, msg: string) {
+  if (process.env.DEBUG) {
+    config.logger.info(msg);
+  }
+}
